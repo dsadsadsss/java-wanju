e7bc636bb3f1ab1ab9561f7276b53028cb04b5b0
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2024-08-15 19:09:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,8 +17,8 @@ export VM_PORT=${VM_PORT:-'8001'} # vmes 端口
 export CF_IP=${CF_IP:-'ip.sb'}  # cf优选
 export SUB_NAME=${SUB_NAME:-'Argo'} # 节点名称
 
-# reality相关设置(不能同时开游戏.nodejs和python端口要改成别的，不能占用玩具端口)
-export SERVER_PORT=${SERVER_PORT:-'8000'} # 端口，玩具自动识别，其他手动修改
+# reality相关设置(注意reality,tuic,hy2协议不能同时开游戏.nodejs和python端口要改成别的，不能占用玩具端口)
+export SERVER_PORT=${SERVER_PORT:-'8000'} # 端口，玩具自动识别，其他手动修改，注意IP不拿被墙
 export SNI=${SNI:-'www.apple.com'} # tls网站
 
 # 游戏相关设置(去掉#开启游戏，复制启动命令填在下面)
