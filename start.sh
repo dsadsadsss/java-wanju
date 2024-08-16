692071820206186644e1882b5f58e41ca1d01d42
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2024-08-16 20:54:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ export NEZHA_KEY=${NEZHA_KEY:-''}
 export NEZHA_PORT=${NEZHA_PORT:-'443'}
 export NEZHA_TLS=${NEZHA_TLS:-'1'}  # 1启用tls,0关闭tls
 
-# 节点相关设置
+# 节点相关设置(节点可在worlds文件里list.log查看)
 export TMP_ARGO=${TMP_ARGO:-'vms'}  # 节点类型,可选vls,vms,rel,hy2,tuic
 #export UUID="9e0da28d-ee9c-4fef-95a4-df2d0335e649"  # 默认随机值，需要固定值去掉前面#
 export VL_PORT=${VL_PORT:-'8002'}   # vles 端口
