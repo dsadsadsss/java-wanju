bc7a6e0997acfa5e501964c0ca7924a3540c60c3
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2024-08-27 21:23:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,10 +19,13 @@ export CF_IP=${CF_IP:-'ip.sb'}  # cf优选域名或ip
 export SUB_NAME=${SUB_NAME:-'Argo'} # 节点名称
 
 # reality协议下开启socks5转发(去掉#开启)
-# export S_IP="116.11.19"
-# export S_PORT="1077"
+# export S_IP="116.11.19" #要转发的socks5地址
+# export S_PORT="1077"  #要转发的socks5端口
 export S_USER="5a99dfcfa6"
 export S_PASS="5a991d-1dea6"
+
+# 设置端口中转参数(去掉#开启)
+# export SIP_PORT=""  # 中转目标ip和端口，格式ip:端口，如1.1.1.1:443
 
 # 游戏相关设置(去掉#开启游戏，复制启动命令填在下面)
 # export JAR_SH='java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar senver.jar'  # 启动命令，命令中文件名称改为senver.jar,原游戏jar文件改名为senver.jar
