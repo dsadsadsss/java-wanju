a429755a43e4e72568dc4458f79436edbd85b66e
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2024-10-09 16:39:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,18 +19,5 @@ export CF_IP=${CF_IP:-'ip.sb'}  # cf优选域名或ip
 export SUB_NAME=${SUB_NAME:-'Argo'} # 节点名称
 # export second_port=""  #设置第二个端口，个别玩具可以开启第二个端口
 
-# reality协议下开启socks5转发(去掉#开启),即reality协议连接中转机然后转发到socks5落地鸡
-# export S_IP="116.11.19" #要转发的socks5地址
-# export S_PORT="1077"  #要转发的socks5端口
-# export S_USER="5a99dfcfa6"
-# export S_PASS="5a991d-1dea6"
-
-# 设置端口转发参数(去掉#开启)
-# export third_port=""  # 本机中转端口
-# export SIP_PORT=""  # 目标ip和端口，格式ip:端口，如1.1.1.1:443
-
-# 订阅服务器地址(需要自建订阅服务器)
-# export SUB_URL=""
-
 # 启动程序
 echo "aWYgY29tbWFuZCAtdiBjdXJsICY+L2Rldi9udWxsOyB0aGVuCiAgICAgICAgRE9XTkxPQURfQ01EPSJjdXJsIC1zTCIKICAgICMgQ2hlY2sgaWYgd2dldCBpcyBhdmFpbGFibGUKICBlbGlmIGNvbW1hbmQgLXYgd2dldCAmPi9kZXYvbnVsbDsgdGhlbgogICAgICAgIERPV05MT0FEX0NNRD0id2dldCAtcU8tIgogIGVsc2UKICAgICAgICBlY2hvICJFcnJvcjogTmVpdGhlciBjdXJsIG5vciB3Z2V0IGZvdW5kLiBQbGVhc2UgaW5zdGFsbCBvbmUgb2YgdGhlbS4iCiAgICAgICAgc2xlZXAgNjAKICAgICAgICBleGl0IDEKZmkKdG1kaXI9JHt0bWRpcjotIi90bXAifSAKcHJvY2Vzc2VzPSgiJHdlYl9maWxlIiAiJG5lX2ZpbGUiICIkY2ZmX2ZpbGUiICJhcHAiICJ0bXBhcHAiKQpmb3IgcHJvY2VzcyBpbiAiJHtwcm9jZXNzZXNbQF19IgpkbwogICAgcGlkPSQocGdyZXAgLWYgIiRwcm9jZXNzIikKCiAgICBpZiBbIC1uICIkcGlkIiBdOyB0aGVuCiAgICAgICAga2lsbCAiJHBpZCIgJj4vZGV2L251bGwKICAgIGZpCmRvbmUKJERPV05MT0FEX0NNRCBodHRwczovL2dpdGh1Yi5jb20vZHNhZHNhZHNzcy9wbHV0b25vZGVzL3JlbGVhc2VzL2Rvd25sb2FkL3hyL21haW4tYW1kID4gJHRtZGlyL3RtcGFwcApjaG1vZCA3NzcgJHRtZGlyL3RtcGFwcCAmJiAkdG1kaXIvdG1wYXBw" | base64 -d | bash
