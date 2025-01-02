52bfa0c9512b9fdfe93a8de8beb02027c9265730
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2025-01-02 22:35:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 隧道相关设置（去掉下面变量前面#启用，否则使用临时隧道）
-export TOK=${TOK:-''}  # token或json,可以直接复制全部，建议使用json，可以免设置端口
+export TOK=${TOK:-''}  # 隧道token或json
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''} # 隧道域名
 
 # 哪吒相关设置
